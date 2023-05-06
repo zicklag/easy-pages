189e5d87278b4f1463ebafebb93aa6a7e8c8e226
Zicklag <zicklag@katharostech.com>
2023-05-06 14:49:59 -0500

    feat: configure tailwind and add home and about pages.

---
+++ components/Hero.tsx
@@ -0,0 +1,33 @@
+import IconChevronRight from "https://deno.land/x/tabler_icons_tsx@0.0.2/tsx/chevron-right.tsx";
+
+export default function Hero() {
+  return (
+    <div
+      class="w-full flex px-8 h-96 justify-center items-center flex-col gap-8 bg-cover bg-center bg-no-repeat bg-gray-100 rounded-xl text-white"
+      style="background-image:linear-gradient(rgba(0, 0, 40, 0.8),rgba(0, 0, 40, 0.8)), url('/gallery/hero-bg.webp');"
+    >
+      <div class="space-y-4 text-center">
+        <h1 class="text-4xl inline-block font-bold">Easy Pages</h1>
+        <p class="text-xl max-w-lg text-blue-100">
+          The Easiest possible workflow to deploy simple, static sites to multiple backends.
+        </p>
+      </div>
+
+      <div>
+        <a
+          href="#"
+          class="block mt-4 text-blue-500 cursor-pointer inline-flex items-center group text-blue-800 bg-white px-8 py-2 rounded-md hover:bg-blue-50 font-bold"
+        >
+          Get Started{" "}
+        </a>
+        <a
+          href="/about"
+          class="block mt-4 transition-colors text-blue-400 cursor-pointer inline-flex items-center group px-4 py-2 hover:text-blue-100"
+        >
+          About{" "}
+          <IconChevronRight class="inline-block w-5 h-5 transition group-hover:translate-x-0.5" />
+        </a>
+      </div>
+    </div>
+  );
+}
--- twind.config.ts
+++ twind.config.ts
@@ -1,5 +1,14 @@
-import { Options } from "$fresh/plugins/twind.ts";
+import { Options } from "$fresh/plugins/twindv1.ts";
+import { defineConfig } from "@twind/core";
+import presetTailwind from "@twind/preset-tailwind";
+import presetTypography from '@twind/preset-typography'
+
+const config = defineConfig({
+  presets: [presetTailwind(), presetTypography()],
+  // deno-lint-ignore no-explicit-any
+}) as any;
 
 export default {
   selfURL: import.meta.url,
+  ...config,
 } as Options;
